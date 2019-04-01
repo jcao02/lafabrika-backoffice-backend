c205ecfa5204627e3260849891975288f69f6877
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-03-31 20:30:53 -0400

    add sqitch user and registry to database

--- services/database/docker-entrypoint-initdb.d/initdb.sh
+++ services/database/docker-entrypoint-initdb.d/initdb.sh
@@ -11,3 +11,14 @@ psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE lafabrika_development TO l
 echo "Initializing lafabrika test database...";
 psql -U postgres -c "CREATE DATABASE lafabrika_test OWNER lafabrika";
 psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE lafabrika_test TO lafabrika";
+
+echo "Initalizing sqitch role..."
+psql -U postgres -c "CREATE ROLE sqitch WITH PASSWORD 'sqitch'";
+psql -U postgres -c "ALTER ROLE sqitch WITH LOGIN";
+psql -U postgres -c "ALTER ROLE sqitch CREATEDB";
+psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE lafabrika_development TO sqitch";
+psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE lafabrika_test TO sqitch";
+
+echo "Initializing sqitch registry..."
+psql -U postgres -c "CREATE DATABASE sqitch OWNER sqitch";
+psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE sqitch TO sqitch";
