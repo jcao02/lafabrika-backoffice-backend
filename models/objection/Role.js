c732441830e82087f5087fb1eb8d0e7f08d02cc9
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-03 11:30:16 -0400

    add Role model jsonSchema and realtionMappings

--- models/objection/Role.js
+++ models/objection/Role.js
@@ -6,6 +6,28 @@ class Role extends Model {
   static get tableName() {
     return 'roles';
   }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      properties: {
+        name: { type: 'string' }
+      }
+    }
+  }
+
+  static get relationMappings() {
+    return {
+      users: {
+        relation: Model.HasManyRelation,
+        modelClass: __dirname + '/User',
+        join: {
+          from: 'roles.name',
+          to: 'users.role'
+        }
+      }
+    };
+  }
 }
 
 module.exports = Role;
