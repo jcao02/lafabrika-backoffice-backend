2e34d6b1be3b80fc39f2072db0a7540b2337e8b0
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-03 11:22:49 -0400

    add jsonSchema to User model

--- models/objection/User.js
+++ models/objection/User.js
@@ -6,6 +6,18 @@ class User extends Model {
   static get tableName() {
     return 'users';
   }
+
+  static get jsonSchema() {
+    return {
+      type: 'object',
+      properties: {
+        id: { type: 'integer' },
+        email: { type: 'string' },
+        firstName: { type: 'string' },
+        lastName: { type: 'string' }
+      }
+    };
+  }
 }
 
 module.exports = User;
