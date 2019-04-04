7c120535533c3a5c91fffc6863840abd3a75ebae
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-04 10:41:17 -0400

    refactor mount.sh and add network creation

--- mount.sh
+++ mount.sh
@@ -1,11 +1,35 @@
 #!/bin/bash
 
-currDir=$(pwd);
 
-databaseDir=$(pwd)/services/database
+# Builds the database image with custom tag
+build_database_image() {
+  echo -n "Building database image $1...";
 
-cd $databaseDir;
-docker build . -t lafabrika/database:0.0.1
+  currDir=$(pwd);
 
-cd $currDir;
+  databaseDir=$currDir/services/database
+
+  cd $databaseDir;
+  docker build . -t $1 &> /dev/null;
+
+  cd $currDir;
+
+  echo "Done!";
+}
+
+# Creates a docker bridge network with custom name
+create_internal_network() {
+  echo -n "Creating network $1...";
+
+  docker network create $1 &> /dev/null;
+
+  echo "Done!";
+}
+
+main() {
+  build_database_image lafabrika/database:0.0.1;
+  create_internal_network internal;
+}
+
+main;
 
