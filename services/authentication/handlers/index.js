430a586c8f001ff7b971a64c2b0a4d1f79cf275d
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-04 18:46:39 -0400

    refactor tests and add success case

--- services/authentication/handlers/index.js
+++ services/authentication/handlers/index.js
@@ -2,17 +2,20 @@
 
 const { User } = require('lafabrika-objection-models');
 const Boom = require('boom');
+const jwt = require('jsonwebtoken');
 
 const signInHandler = async (request, h) => {
   const { email, password } = request.payload;
+
   const isRightCombination = await User.checkUserPassword(email, password);
 
   if (isRightCombination) {
-    return "";
+    const user = await User.query().where('email', '=', email);
+    const token = jwt.sign({ email, role: user.role }, process.env.JWT_PRIV_KEY);
+    return h.response({ token }).code(201);
   } else {
     throw Boom.unauthorized('Wrong email/password combination');
   }
-
 };
 
 module.exports = {
--- services/authentication/spec/handlers/index.spec.js
+++ services/authentication/spec/handlers/index.spec.js
@@ -1,6 +1,17 @@
 const { server } = require('../../server');
 const { User } = require('lafabrika-objection-models');
 
+// Helper to test status code
+async function testStatusCode(opts, expectedCode, cb) {
+  try {
+    const result = await server.inject(opts);
+    expect(result.statusCode).toBe(expectedCode)
+    cb(result);
+  } catch (err) {
+    cb(null, err);
+  }
+}
+
 describe('Handlers', () => {
   describe('signInHandler', () => {
     const email = 'myEmail@example.com';
@@ -12,14 +23,9 @@ describe('Handlers', () => {
         url: '/sign-in',
         payload: { password }
       }
-      try {
-        const result = await server.inject(opts);
-        const expectedCode = 400;
-        expect(result.statusCode).toBe(expectedCode)
-        done();
-      } catch (err) {
-        done.fail(err);
-      }
+
+      const expectedCode = 400;
+      testStatusCode(opts, expectedCode, (_, err) => !err ? done() : done.fail(err));
     });
     it('should return 400 when password is missing', async done => {
       const opts = {
@@ -27,14 +33,8 @@ describe('Handlers', () => {
         url: '/sign-in',
         payload: { email }
       }
-      try {
-        const result = await server.inject(opts);
-        const expectedCode = 400;
-        expect(result.statusCode).toBe(expectedCode)
-        done();
-      } catch (err) {
-        done.fail(err);
-      }
+      const expectedCode = 400;
+      testStatusCode(opts, expectedCode, (_, err) => !err ? done() : done.fail(err));
     });
     it('should return 401 when email/password combination is wrong', async done => {
       const opts = {
@@ -42,18 +42,16 @@ describe('Handlers', () => {
         url: '/sign-in',
         payload: { email, password }
       }
-      try {
-        const modelSpy = spyOn(User, 'checkUserPassword').and.returnValue(Promise.resolve(false));
-        const result = await server.inject(opts);
-
-        const expectedCode = 401;
-        expect(result.statusCode).toBe(expectedCode)
-
-        expect(modelSpy).toHaveBeenCalledWith(email, password);
-        done();
-      } catch (err) {
-        done.fail(err);
-      }
+      const modelSpy = spyOn(User, 'checkUserPassword').and.returnValue(Promise.resolve(false));
+      const expectedCode = 401;
+      testStatusCode(opts, expectedCode, (_, err) => {
+        if (err) {
+          done.fail(err);
+        } else {
+          expect(modelSpy).toHaveBeenCalledWith(email, password);
+          done();
+        }
+      });
     });
     it('should return 201 with jwt token when email/password combination is right', async done => {
       const opts = {
@@ -61,22 +59,26 @@ describe('Handlers', () => {
         url: '/sign-in',
         payload: { email, password }
       }
-      try {
-        const modelSpy = spyOn(User, 'checkUserPassword').and.returnValue(Promise.resolve(true));
-        const result = await server.inject(opts);
 
+      process.env.JWT_PRIV_KEY = 'JWT_PRIV_KEY';
+      const modelSpy = spyOn(User, 'checkUserPassword').and.returnValue(Promise.resolve(true));
+      const userQuery = { where() { return Promise.resolve({ email, role: 'admin' }) } };
+      const querySpy = spyOn(User, 'query').and.returnValue(userQuery);
+      const expectedCode = 201;
+      testStatusCode(opts, expectedCode, (res, err) => {
+        if (err) {
+          done.fail(err);
+        } else {
+          const payload = JSON.parse(res.payload);
+          expect(payload.token).not.toBeUndefined();
 
-        const expectedCode = 201;
-        expect(result.statusCode).toBe(expectedCode)
+          expect(modelSpy).toHaveBeenCalledWith(email, password);
+          expect(querySpy).toHaveBeenCalled();
 
-        const payload = JSON.parse(result.payload);
-
-        expect(payload.token).not.toBeUndefined();
-        expect(modelSpy).toHaveBeenCalledWith(email, password);
-        done();
-      } catch (err) {
-        done.fail(err);
-      }
+          delete process.env.JWT_PRIV_KEY;
+          done();
+        }
+      });
     });
   });
 });
