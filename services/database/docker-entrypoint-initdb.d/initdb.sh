fef2e4ee0be7f15514c1d83be49fdcc1c42aba98
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-04 11:51:45 -0400

    fix(database, migrations): ephemeral migrations, bridge network and database config (#18)
    * remove migrations service
    
    * add docker-compose-migrations.yml to run ephemeral migrations
    
    * remove unnecessary script
    
    * move migrations docker-compose to service
    
    * use bridge netowrk to connect migrations to database
    
    * refactor mount.sh and add network creation
    
    * create pg-data directory in mount.sh
    
    * set SUPERUSER to sqitch role to create extensions and remove unnecessary permissions
    
    * remove unnecessary test database
    
    * authorize lafabrika role to use lafabrika schema
    
    * add .gitignore
    
    * add missing EOF line

--- services/database/docker-entrypoint-initdb.d/initdb.sh
+++ services/database/docker-entrypoint-initdb.d/initdb.sh
@@ -4,21 +4,11 @@ psql -U postgres -c "CREATE ROLE lafabrika WITH PASSWORD 'lafabrika'";
 psql -U postgres -c "ALTER ROLE lafabrika WITH LOGIN";
 psql -U postgres -c "ALTER ROLE lafabrika CREATEDB";
 
-echo "Initializing lafabrika development database...";
-psql -U postgres -c "CREATE DATABASE lafabrika_development OWNER lafabrika";
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE lafabrika_development TO lafabrika";
-
-echo "Initializing lafabrika test database...";
-psql -U postgres -c "CREATE DATABASE lafabrika_test OWNER lafabrika";
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE lafabrika_test TO lafabrika";
+echo "Initializing lafabrika database...";
+psql -U postgres -c "CREATE DATABASE lafabrika OWNER lafabrika";
+psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE lafabrika TO lafabrika";
 
 echo "Initalizing sqitch role..."
 psql -U postgres -c "CREATE ROLE sqitch WITH PASSWORD 'sqitch'";
 psql -U postgres -c "ALTER ROLE sqitch WITH LOGIN";
-psql -U postgres -c "ALTER ROLE sqitch CREATEDB";
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE lafabrika_development TO sqitch";
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE lafabrika_test TO sqitch";
-
-echo "Initializing sqitch registry..."
-psql -U postgres -c "CREATE DATABASE sqitch OWNER sqitch";
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE sqitch TO sqitch";
+psql -U postgres -c "ALTER ROLE sqitch WITH SUPERUSER";
--- services/migrations/sqitch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker-compose exec -u `id -u $USER` migrations sqitch $@;
