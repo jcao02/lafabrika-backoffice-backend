13f32179feea3e0799833af63ecf48d8e0ce597d
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-04 11:13:47 -0400

    remove unnecessary test database

--- services/database/docker-entrypoint-initdb.d/initdb.sh
+++ services/database/docker-entrypoint-initdb.d/initdb.sh
@@ -4,13 +4,9 @@ psql -U postgres -c "CREATE ROLE lafabrika WITH PASSWORD 'lafabrika'";
 psql -U postgres -c "ALTER ROLE lafabrika WITH LOGIN";
 psql -U postgres -c "ALTER ROLE lafabrika CREATEDB";
 
-echo "Initializing lafabrika development database...";
-psql -U postgres -c "CREATE DATABASE lafabrika_development OWNER lafabrika";
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE lafabrika_development TO lafabrika";
-
-echo "Initializing lafabrika test database...";
-psql -U postgres -c "CREATE DATABASE lafabrika_test OWNER lafabrika";
-psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE lafabrika_test TO lafabrika";
+echo "Initializing lafabrika database...";
+psql -U postgres -c "CREATE DATABASE lafabrika OWNER lafabrika";
+psql -U postgres -c "GRANT ALL PRIVILEGES ON DATABASE lafabrika TO lafabrika";
 
 echo "Initalizing sqitch role..."
 psql -U postgres -c "CREATE ROLE sqitch WITH PASSWORD 'sqitch'";
