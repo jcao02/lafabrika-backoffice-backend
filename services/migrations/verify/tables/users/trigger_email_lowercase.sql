4a1cb105a6db8ed5aa964bde1d2965b9daa0fec2
Daniel Arocha <arocha.daniel3@gmail.com>
2019-04-10 15:03:55 -0400

    configure verify file for trigger

--- services/migrations/verify/tables/users/trigger_email_lowercase.sql
+++ services/migrations/verify/tables/users/trigger_email_lowercase.sql
@@ -2,6 +2,12 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+SET search_path TO lafabrika, public;
+
+SELECT 1/count(*)
+FROM information_schema.triggers
+WHERE trigger_name = 'email_lowercase'
+  AND event_object_table = 'users'
+  AND event_manipulation = 'INSERT';
 
 ROLLBACK;
