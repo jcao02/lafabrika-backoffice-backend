31415aef282ffb8923f901d119e8869bd8758a74
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-13 17:07:06 -0400

    fix(authentication): add env variables and network, and fix minor errors in service (#29)
    * add missing env variables and internal network
    
    * get first user in authentication manager
    
    * add proper try-catch in auth handler
    
    * add CORS support in auth server

--- services/authentication/handlers/index.js
+++ services/authentication/handlers/index.js
@@ -6,14 +6,19 @@ const Boom = require('boom');
 
 const signInHandler = async (request, h) => {
   const { email, password } = request.payload;
-  const userOrNull = await AuthenticationManager.authenticate(email, password);
+  try {
+    const userOrNull = await AuthenticationManager.authenticate(email, password);
 
-  if (userOrNull) {
-    const expiresIn = '1y'; // One year expiration
-    const token = TokenGenerator.generate({ email: userOrNull.email, role: userOrNull.role }, expiresIn);
-    return h.response({ token }).code(201);
-  } else {
-    throw Boom.unauthorized('Wrong email/password combination');
+    if (userOrNull) {
+      const expiresIn = '1y'; // One year expiration
+      const payload = { id: userOrNull.id, email: userOrNull.email, role: userOrNull.role };
+      const token = TokenGenerator.generate(payload, expiresIn);
+      return h.response({ token }).code(201);
+    } else {
+      throw Boom.unauthorized('Wrong email/password combination');
+    }
+  } catch (err) {
+    throw err;
   }
 };
 
--- services/authentication/server.js
+++ services/authentication/server.js
@@ -5,7 +5,10 @@ const routes = require('./routes');
 
 const server = Hapi.server({
   port: 3000,
-  host: '0.0.0.0'
+  host: '0.0.0.0',
+  routes: {
+    cors: { credentials: true }
+  }
 });
 
 routes.forEach((route) => {
