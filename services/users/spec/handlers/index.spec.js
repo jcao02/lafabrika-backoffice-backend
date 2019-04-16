fb6164e8f660cac11689cfa06908471a4cd8486e
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-16 10:06:31 -0400

    enclose error specs in Errors describe

--- services/users/spec/handlers/index.spec.js
+++ services/users/spec/handlers/index.spec.js
@@ -14,122 +14,124 @@ describe('Users Handlers', () => {
   });
 
   describe('POST /admin/users', () => {
-    it('should return 401 if no authentication', async done => {
-      const opts = {
-        method: 'POST',
-        url: '/admin/users',
-        payload: { ...user }
-      }
-      const result = await server.inject(opts);
-      const expected = 401;
-      expect(result.statusCode).toBe(expected)
-      done();
-    });
-    it('should return 403 if no admin', async done => {
-      const opts = {
-        method: 'POST',
-        url: '/admin/users',
-        payload: { ...user },
-        auth: {
-          strategy: 'jwt',
-          credentials: { scope: userScope }
+    describe('Errors', () => {
+      it('should return 401 if no authentication', async done => {
+        const opts = {
+          method: 'POST',
+          url: '/admin/users',
+          payload: { ...user }
         }
-      };
-      const result = await server.inject(opts);
-      const expected = 403;
-      expect(result.statusCode).toBe(expected)
-      done();
-    });
+        const result = await server.inject(opts);
+        const expected = 401;
+        expect(result.statusCode).toBe(expected)
+        done();
+      });
+      it('should return 403 if no admin', async done => {
+        const opts = {
+          method: 'POST',
+          url: '/admin/users',
+          payload: { ...user },
+          auth: {
+            strategy: 'jwt',
+            credentials: { scope: userScope }
+          }
+        };
+        const result = await server.inject(opts);
+        const expected = 403;
+        expect(result.statusCode).toBe(expected)
+        done();
+      });
 
-    it('should validate email as required', async done => {
-      const opts = {
-        method: 'POST',
-        url: '/admin/users',
-        payload: { password: user.password, role: user.role },
-        auth: {
-          strategy: 'jwt',
-          credentials: { scope: adminScope }
-        }
-      };
-      const result = await server.inject(opts);
-      const expected = 400;
-      expect(result.statusCode).toBe(expected)
-      done();
-    });
-    it('should validate email', async done => {
-      const opts = {
-        method: 'POST',
-        url: '/admin/users',
-        payload: { ...user, email: 'invalid' },
-        auth: {
-          strategy: 'jwt',
-          credentials: { scope: adminScope }
-        }
-      };
-      const result = await server.inject(opts);
-      const expected = 400;
-      expect(result.statusCode).toBe(expected)
-      done();
-    });
-    it('should validate role as required', async done => {
-      const opts = {
-        method: 'POST',
-        url: '/admin/users',
-        payload: { email: user.email, password: user.password },
-        auth: {
-          strategy: 'jwt',
-          credentials: { scope: adminScope }
-        }
-      };
-      const result = await server.inject(opts);
-      const expected = 400;
-      expect(result.statusCode).toBe(expected)
-      done();
-    });
-    it('should validate role', async done => {
-      const opts = {
-        method: 'POST',
-        url: '/admin/users',
-        payload: { ...user, role: 'invalid' },
-        auth: {
-          strategy: 'jwt',
-          credentials: { scope: adminScope }
-        }
-      };
-      const result = await server.inject(opts);
-      const expected = 400;
-      expect(result.statusCode).toBe(expected)
-      done();
-    });
-    it('should validate password as required', async done => {
-      const opts = {
-        method: 'POST',
-        url: '/admin/users',
-        payload: { email: user.email, role: 'user' },
-        auth: {
-          strategy: 'jwt',
-          credentials: { scope: adminScope }
-        }
-      };
-      const result = await server.inject(opts);
-      const expected = 400;
-      expect(result.statusCode).toBe(expected)
-      done();
-    });
-    it('should validate password min-length 8', async done => {
-      const opts = {
-        method: 'POST',
-        url: '/admin/users',
-        payload: { email: user.email, role: 'user', password: '1234567' },
-        auth: {
-          strategy: 'jwt',
-          credentials: { scope: adminScope }
-        }
-      };
-      const result = await server.inject(opts);
-      const expected = 400;
-      expect(result.statusCode).toBe(expected)
-      done();
+      it('should validate email as required', async done => {
+        const opts = {
+          method: 'POST',
+          url: '/admin/users',
+          payload: { password: user.password, role: user.role },
+          auth: {
+            strategy: 'jwt',
+            credentials: { scope: adminScope }
+          }
+        };
+        const result = await server.inject(opts);
+        const expected = 400;
+        expect(result.statusCode).toBe(expected)
+        done();
+      });
+      it('should validate email', async done => {
+        const opts = {
+          method: 'POST',
+          url: '/admin/users',
+          payload: { ...user, email: 'invalid' },
+          auth: {
+            strategy: 'jwt',
+            credentials: { scope: adminScope }
+          }
+        };
+        const result = await server.inject(opts);
+        const expected = 400;
+        expect(result.statusCode).toBe(expected)
+        done();
+      });
+      it('should validate role as required', async done => {
+        const opts = {
+          method: 'POST',
+          url: '/admin/users',
+          payload: { email: user.email, password: user.password },
+          auth: {
+            strategy: 'jwt',
+            credentials: { scope: adminScope }
+          }
+        };
+        const result = await server.inject(opts);
+        const expected = 400;
+        expect(result.statusCode).toBe(expected)
+        done();
+      });
+      it('should validate role', async done => {
+        const opts = {
+          method: 'POST',
+          url: '/admin/users',
+          payload: { ...user, role: 'invalid' },
+          auth: {
+            strategy: 'jwt',
+            credentials: { scope: adminScope }
+          }
+        };
+        const result = await server.inject(opts);
+        const expected = 400;
+        expect(result.statusCode).toBe(expected)
+        done();
+      });
+      it('should validate password as required', async done => {
+        const opts = {
+          method: 'POST',
+          url: '/admin/users',
+          payload: { email: user.email, role: 'user' },
+          auth: {
+            strategy: 'jwt',
+            credentials: { scope: adminScope }
+          }
+        };
+        const result = await server.inject(opts);
+        const expected = 400;
+        expect(result.statusCode).toBe(expected)
+        done();
+      });
+      it('should validate password min-length 8', async done => {
+        const opts = {
+          method: 'POST',
+          url: '/admin/users',
+          payload: { email: user.email, role: 'user', password: '1234567' },
+          auth: {
+            strategy: 'jwt',
+            credentials: { scope: adminScope }
+          }
+        };
+        const result = await server.inject(opts);
+        const expected = 400;
+        expect(result.statusCode).toBe(expected)
+        done();
+      });
     });
   });
 });
