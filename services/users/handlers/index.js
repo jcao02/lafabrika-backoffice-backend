d0373c4ff8f238d375c22d2a3b3f283c051ea0e8
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-15 20:12:52 -0400

    add handlers basic functionality

--- services/users/handlers/index.js
+++ services/users/handlers/index.js
@@ -1,18 +1,53 @@
 'use strict';
 
+const UserManager = require('../classes/user-manager');
+
+/**
+ * Handles user creation
+ */
 const userNewHandler = async (request, h) => {
+  const manager = new UserManager();
+  const user = await manager.createUser(request.payload);
+  return h.response(user.toJSON()).code(201);
 };
 
-const userEditHandler = async (request, h) => {
+/**
+ * Handles user edition
+ */
+const userEditHandler = async (request) => {
+  const { id } = request.params;
+  const manager = new UserManager();
+  const user = await manager.updateUser(id, request.payload);
+  return user.toJSON();
 };
 
-const userShowHandler = async (request, h) => {
+/**
+ * Handles user show
+ */
+const userShowHandler = async (request) => {
+  const { id } = request.params;
+  const manager = new UserManager();
+  const user = await manager.getUser(id);
+  return user.toJSON();
 };
 
-const userListHandler = async (request, h) => {
+/**
+ * Handles user list
+ */
+const userListHandler = async (request) => {
+  const { limit, offset = 0 } = request.query;
+  const manager = new UserManager();
+  const users = manager.getUsers({ limit, offset });
+  return users.toJSON();
 };
 
-const userDeleteHandler = async (request, h) => {
+/**
+ * Handles user delete
+ */
+const userDeleteHandler = async (request) => {
+  const { id } = request.params;
+  const manager = new UserManager();
+  return await manager.deleteUser(id);
 };
 
 
