0874a71108d5c2a77ebc281dd0ab62ccc5ab88be
Juan Carlos Arocha <arochajuan2@gmail.com>
2019-04-17 17:30:15 -0400

    fix(migrations): add password encryption on UPDATE, fix email_lowercase function typo and add privileges to role lafabrika (#38)
    * add password encryption trigger on users UPDATE
    
    * fix error in email_lowercase function
    
    * add privileges to role lafabrika on schema la fabrika

---
+++ services/migrations/deploy/tables/users/trigger_encrypt_password_update.sql
@@ -0,0 +1,13 @@
+-- Deploy lafabrika:tables/users/trigger_encrypt_password_update to pg
+
+BEGIN;
+
+SET search_path TO lafabrika, public;
+
+CREATE TRIGGER encrypt_password_edit
+BEFORE UPDATE ON users
+FOR EACH ROW
+WHEN (NEW.password <> OLD.password)
+EXECUTE PROCEDURE encrypt_user_password();
+
+COMMIT;
---
+++ services/migrations/verify/tables/users/trigger_encrypt_password_update.sql
@@ -0,0 +1,13 @@
+-- Verify lafabrika:tables/users/trigger_encrypt_password_update on pg
+
+BEGIN;
+
+SET search_path TO lafabrika, public;
+
+SELECT 1/count(*)
+FROM information_schema.triggers
+WHERE trigger_name = 'encrypt_password_edit'
+  AND event_object_table = 'users'
+  AND event_manipulation = 'UPDATE';
+
+ROLLBACK;
